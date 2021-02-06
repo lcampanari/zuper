e3f02a0cdba8a7f2edfa26b58f17e117f305ddd5
Lucas Campanari <campanari.ls@gmail.com>
2021-02-06 15:33:42 -0500

    feat: add hit effect

---
+++ HitHurtBoxes/Hurtbox.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+export(bool) var show_hit = true
+
+const HitEffect = preload("res://Effects/HitEffect.tscn")
+
+
+func _on_Hurtbox_area_entered(area):
+	if show_hit:
+		show_hit_effect()
+
+func show_hit_effect():
+	var effect = HitEffect.instance()
+	var main = get_tree().current_scene
+	main.add_child(effect)
+	effect.global_position = global_position - Vector2(0, 8)
