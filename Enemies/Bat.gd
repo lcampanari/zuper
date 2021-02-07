cac89c5b7d750a0966ea2db9d28f22c28edc28b4
Lucas Campanari <campanari.ls@gmail.com>
2021-02-06 20:47:49 -0500

    feat: add detection zone for enemies

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -2,16 +2,41 @@ extends KinematicBody2D
 
 const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
-var knockback = Vector2.ZERO
+export var ACCELERATION = 300
+export var MAX_SPEED = 50
+export var FRICTION = 200
 
+
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+
+var velocity = Vector2.ZERO
+var knockback = Vector2.ZERO
+var state = CHASE
+
+onready var sprite = $AnimatedSprite
 onready var stats = $Stats
+onready var playerDetectionZone = $PlayerDetectionZone
 
 func _ready():
 	print(stats.health)
 
 func _physics_process(delta):
-	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
+	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE: 
+			idle_state(delta)
+		WANDER:
+			wander_state(delta)
+		CHASE:
+			chase_state(delta)
+	
+	velocity = move_and_slide(velocity)
 	
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
@@ -22,3 +47,23 @@ func _on_Stats_no_health():
 	var enemyDeathEffect = EnemyDeathEffect.instance()
 	get_parent().add_child(enemyDeathEffect)
 	enemyDeathEffect.global_position = global_position
+
+func seek_player():
+	if playerDetectionZone.can_see_player():
+		state = CHASE
+
+func idle_state(delta):
+	velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	seek_player()
+	
+func wander_state(delta):
+	pass
+	
+func chase_state(delta):
+	var player = playerDetectionZone.player
+	if player != null:
+		var direction = (player.global_position - global_position).normalized()
+		velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+	else:
+		state = IDLE
+	sprite.flip_h = velocity.x < 0
